fcc3bdfce2baa9df1759b1376edd5e31579f1ad5
najmanmna <139351540+najmanmna@users.noreply.github.com>
2025-09-18 13:22:53 +0530

    favicon added

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
